5dac31e3b69e75047d95e1cd20fbc1f27b6b14c1
Bobur <bobur0218programmer@gmail.com>
2024-01-27 22:55:44 +0500

    start

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+const qs = tag => document.querySelector(tag);
+const qsAll = tag => document.querySelectorAll(tag);
+const getID = tag => document.getElementById(tag);
+
+window.addEventListener("DOMContentLoaded", function() 
+{
+
+});
