ca1894ea66f447785e3d8365ba13b445f08fac6c
Bobur <bobur0218programmer@gmail.com>
2024-01-28 11:01:52 +0500

    type of breaks and timers is added

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,11 @@
+"use strict";
+
 const qs = tag => document.querySelector(tag);
 const qsAll = tag => document.querySelectorAll(tag);
 const getID = tag => document.getElementById(tag);
 
 let timer;
-let minutes = 25;
+let minutes;
 let seconds = 0;
 let isRunning = false;
 
@@ -14,10 +16,10 @@ function timerCountDown ()
     {
         seconds = 60;
         minutes--;
-        minDisplay.textContent = minutes;
+        minDisplay.textContent = String(minutes).padStart(2, "0");
     }
     seconds--;
-    secDisplay.textContent = seconds;
+    secDisplay.textContent = String(seconds).padStart(2, "0");
 }
 
 // Toggle Timer Function
@@ -37,11 +39,26 @@ function toggleTimer ()
     }
 }
 
+// Type Time Function
+function setTime (mins)
+{
+    minutes = mins;
+    seconds = 0;
+    minDisplay.textContent = String(minutes).padStart(2, "0");
+    secDisplay.textContent = String(seconds).padStart(2, "0");
+}
+
 window.addEventListener("DOMContentLoaded", function() 
 {
     const minDisplay = getID("minDisplay");
     const secDisplay = getID("secDisplay");
     const btnToggle = getID("btnToggle");
+    const btnShortBreak = getID("btnShortBreak");
+    const btnPomodoro = getID("btnPomodoro");
+    const btnLongBreak = getID("btnLongBreak");
 
     btnToggle.addEventListener("click", toggleTimer);
+    btnShortBreak.addEventListener("click", () => setTime(5)); 
+    btnPomodoro.addEventListener("click", () => setTime(25)); 
+    btnLongBreak.addEventListener("click", () => setTime(15)); 
 });
